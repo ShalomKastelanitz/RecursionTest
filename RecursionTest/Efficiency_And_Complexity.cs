2b74dc8385da09b43a2c4041a409d64bb2801aeb
This User <This User@DESKTOP-L9S4R74>
2024-09-08 20:40:49 +0300

    Add project files.

---
+++ RecursionTest/Efficiency_And_Complexity.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionTest
+{
+    internal class Efficiency_And_Complexity
+    {
+        //question1
+
+        public static int CalculateOddSum(int[] array)//O(n)
+        {
+            int sum = 0;
+            foreach (int num in array)
+            {
+                if (num % 2 != 0)
+                {
+                    sum += num;
+                }
+            }
+            return sum;
+        }
+
+        //question2
+
+        public static (int, int) FindMaximumTwo(int[] array)//O(n)
+        {
+            int maxSum = int.MinValue;
+            int num1 = 0, num2 = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int sum = array[i] + array[i + 1];
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    num1 = array[i];
+                    num2 = array[i + 1];
+                }
+            }
+
+            return (num1, num2);
+        }
+        //question3
+
+        public static bool ContainsNumber(int[] array, int number)// O(n)
+        {
+            foreach (int num in array)
+            {
+                if (num == number)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //question4
+
+        public static int SumOfAllTriplets(int[] array)//O(n)
+        {
+            int maxSum = int.MinValue;
+
+            for (int i = 0; i < array.Length - 2; i++)
+            {
+                int sum = array[i] + array[i + 1] + array[i + 2];
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+            }
+
+            return maxSum;
+        }
+        //question5
+
+        public static (int, int) FindLastPairWithProduct(int[] array, int product)//O(n)
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                if (array[i] * array[i - 1] == product)
+                {
+                    return (array[i - 1], array[i]);
+                }
+            }
+
+            return (-1, -1); // Indicate no pair was found
+        }
+        //question6
+        public static int CountEvenNumbers(int[] array)
+        {
+            int count = 0;
+            foreach (int num in array)
+            {
+                if (num % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        //question8
+        public static void FindPairsWithSum(int[] array, int sum)//O(n^2)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] + array[j] == sum)
+                    {
+                        Console.WriteLine($"Pair: ({array[i]}, {array[j]})");
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ RecursionTest/RecursionTest.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+public class RecursionTest
+{
+    public static void Main(string[] args)
+    {
+        //int[] arr = {1, 2, 3, 4, 5}; 
+        //Console.WriteLine(multiply(4, 3));
+        //Console.WriteLine(sumOfDigits(467));
+        //Console.WriteLine(power(5,3));
+        //reversed(423);
+        //Console.WriteLine("");
+        //Console.WriteLine(findSum(arr,4));
+        Console.WriteLine(divide(15,4)); 
+        Console.WriteLine(isPalindrome("ababav"));
+    }
+    //question1
+    public static int multiply(int a, int b)
+    {
+        if (b == 0)
+        {
+            return 0;
+        }
+        return a + multiply(a, b - 1);
+
+    }
+    //question2
+      public static int sumOfDigits(int num)
+    {
+        if(num == 0)
+        {
+            return 0;
+        }
+        
+        return (num%10)+sumOfDigits(num/10);
+
+    }
+    //question3
+
+    public static int power(int a, int b)
+    {
+        if (b == 0)
+        {
+            return 1;
+        }
+        return a* power(a, b-1);
+    }
+    //question4
+    public static void reversed(int num)
+    {
+        if (num == 0)
+        {
+            return;
+        }
+        Console.Write(num%10);
+        reversed (num/10);
+    }
+    //question5
+
+    public static int findSum(int[] arr, int Length)
+    {
+        if(Length == 0)
+        {
+            return arr[Length];
+        }
+       int sum = arr[Length];
+        return sum+=  findSum(arr, Length-1);
+    }
+    //question6
+    public static int divide(int a, int b)
+    {
+        if (a < 1)
+        {
+            return 0;
+
+        }
+        int cnt = 0;
+        if (a - b >= 0)
+        {
+            cnt = 1;
+        }
+        return cnt+divide(a-b, b);
+    }
+    //question7
+
+    public static bool isPalindrome(string str)
+    {
+        
+        if (str.Length <=1)
+        {
+            return true;
+        }
+        bool Palindrome=false;
+        if (str[0] == str[str.Length - 1])
+        {
+            Palindrome = true;
+        }
+        else
+        {
+            return Palindrome;
+        }
+        string str2 = str.Substring(1, str.Length - 2);
+        return Palindrome = isPalindrome(str2);
+    }
+    //question8
+
+   public static string DecimalToBinary(int n)
+    {
+        if (n == 0)
+        {
+            return "0";
+        }
+        if (n == 1)
+        {
+            return "1";
+        }
+
+        return DecimalToBinary(n / 2) + (n % 2).ToString();
+    }
+    //question9
+  public static int GCD(int a, int b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+
+        return GCD(b, a % b);
+    }
+
+
+}
